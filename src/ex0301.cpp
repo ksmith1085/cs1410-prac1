786d2b3d14d752932e4fd611dbdea043645aa48e
ksmith1085 <kylesmith2@mail.weber.edu>
2025-05-26 22:53:43 +0000

    VA3 Functions

---
+++ src/ex0301.cpp
@@ -0,0 +1,36 @@
+# include <iostream> 
+
+using namespace std;
+string repChar(char, int); //by putting that in, that repchar can use both
+long hms_2_secs(int h, int m, int s);
+void swapv(double& x, double& y);
+
+void swapv(double& x, double& y){
+    double t = x;
+    x = y;
+    y = t;
+}
+
+long hms_2_secs(int h, int m, int s){
+    long secs = h * 60 * 60 + m * 60 + s;
+    return secs;
+}
+
+int main(){
+    cout << repChar('X', 8) << endl;
+    cout << hms_2_secs(4, 56, 17) << "seconds" << endl;
+
+    double x = 10.0, y = 30.0;
+    cout << x << ", " << y << endl;
+    swapv(x,y);
+    cout  << x << ", " << y << endl;
+    return 0;
+}
+
+string repChar(char c, int n){
+    string r;
+    for(int i = 0; i < n; i++){
+        r = r + c;
+    }
+    return r;
+}
